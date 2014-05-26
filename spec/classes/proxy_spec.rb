81de43f4dc7cae01f27ccf48a568dcc65dcc9406
Alex Tu <alex.lei.tu@gmail.com>
2014-05-26 13:17:33 +0800

    add

--- spec/classes/proxy_spec.rb
+++ spec/classes/proxy_spec.rb
@@ -7,41 +7,38 @@ describe 'proxy' do
      
      describe "proxy.sh on #{system}" do
        it { should contain_file('/etc/profile.d/proxy.sh').with({
-          'context' =>'template('proxy/common.erb')',
           'path'    => '/etc/profile.d/proxy.sh',
           'owner'   => 'root',
           'group'   => 'root',
           'mode'    => '644',
        })
        }
-         context "params" do
-           let(:params) {{
-             :server   => 'http://exaplem.com',
-             :port     => '8080',
-             :no_proxy => '[ '127.0.0.1', 'localhost' ]',
-           }}
-         end
-       end
+     end
+     context "params" do
+       let(:params) {{
+         :server   => 'http://exaplem.com',
+         :port     => '8080',
+         :no_proxy => '[ '127.0.0.1', 'localhost' ]',
+       }}
+      end
      end
 
    dscribe "Suse" do
     let(:facts) {{ :osfamily => SuSE }}
      describe "proxy.sh on #{system}" do
        it { should contain_file('/etc/sysconfig/proxy').with({
-          'context' => 'template('proxy/suse.erb')',
           'path'    => '/etc/sysconfig/proxy',
           'owner'   => 'root',
           'group'   => 'root',
           'mode'    => '644',
        })
-         context "params" do
-           let(:params) {{
-             :server   => 'http://exaplem.com',
-             :port     => '8080',
-             :no_proxy => '[ '127.0.0.1', 'localhost' ]',
-           }}
-       end
      end
+       context "params" do
+         let(:params) {{
+           :server   => 'http://exaplem.com',
+           :port     => '8080',
+           :no_proxy => '[ '127.0.0.1', 'localhost' ]',
+         }}
+       end
    end
-
 end
