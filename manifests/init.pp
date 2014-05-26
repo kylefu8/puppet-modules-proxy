f8dddd77c918ee891a234977005de3d249d0608e
Alex Tu <alex.lei.tu@gmail.com>
2014-05-26 12:42:15 +0800

    remvoe space

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,57 +15,52 @@ class proxy (
 ){
 
   validate_array($no_proxy)
-  if empty($server) == true { fail ("server is must") }
-  if empty($port) == true { fail ("port is must") }
-  if is_integer($port) == false { fail( "port must be integers") }
+  if empty($server) == true { fail ('server is must') }
+  if empty($port) == true { fail ('port is must') }
+  if is_integer($port) == false { fail( 'port must be integers') }
 
   $_https_server = $https_server ? {
     'USE_DEFAULTS' => $server,
     default        => $https_server,
   }
-  
   $_https_port = $https_port ? {
     'USE_DEFAULTS' => $port,
     default        => $https_port,
   }
-  
   $_ftp_server = $ftp_server ? {
     'USE_DEFAULTS' => $server,
     default        => $ftp_server,
   }
-
   $_ftp_port   = $ftp_port ? {
     'USE_DEFAULTS' => $port,
     default        => $ftp_port,
   }
-
   $_no_proxy   = $no_proxy ? {
     'USE_DEFAULTS' => join([ '127.0.0.1','localhost' ],", "),
     default        => join($no_proxy,", "),
   }
   
   case $::osfamily {
-    'RedHat', 'Debian': {
-       $proxy_path = '/etc/profile.d/proxy.sh'
-       $template   = 'proxy/common.erb'
+   'RedHat', 'Debian': {
+     $proxy_path = '/etc/profile.d/proxy.sh'
+     $template   = 'proxy/common.erb'
    }
-    'SuSe': {
-       $proxy_path = '/etc/sysconfig/proxy'
-       $template   = 'proxy/suse.erb'
+   'SuSe': {
+     $proxy_path = '/etc/sysconfig/proxy'
+     $template   = 'proxy/suse.erb'
      
-       $_gopher_server = $gopher_server ? {
-          'USE_DEFAULTS' => $server,
-          default        => $gopher_server,
-       }
-
-       $_gopher_port   = $gopher_port ? {
-          'USE_DEFAULTS' => $port,
-          default        => $gopher_port,
-       }
-       validate_re($_gopher_server, '^http', 'The format has to start with http://')
-    }
-    default: {
-      fail("Your $(::osfamily) currently is not supported")
+     $_gopher_server = $gopher_server ? {
+       'USE_DEFAULTS' => $server,
+       default        => $gopher_server,
+     }
+     $_gopher_port   = $gopher_port ? {
+       'USE_DEFAULTS' => $port,
+       default        => $gopher_port,
+     }
+     validate_re($_gopher_server, '^http', 'The format has to start with http://')
+   } 
+   default: {
+     fail("Your $(::osfamily) currently is not supported")
     }
    }
 
